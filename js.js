2aa15fb0b68591d5718c0cfc20ea2f67040ba565
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-02-18 16:54:17 +0100

    Final od Project3

--- js.js
+++ js.js
@@ -1,22 +1,39 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
+const pass = document.querySelector("#password");
+const p = document.querySelector(".passinfo");
 const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
 
+const check = function () {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.textContent = "Twoje hasło jest bardzo mocne";
+		p.style.color = "lime";
+	} else if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers)
+	) {
+		p.textContent = "Twoje hasło jest mocne";
+		p.style.color = "green";
+	} else {
+		p.textContent = "Twoje hasło jest słabe";
+		p.style.color = "red";
+	}
+};
 
-const check = function(){
-  if(pass.value.length >minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)){
-    p.textContent='Twoje hasło jest bardzo mocne';
-    p.style.color='lime'
-  }else if (pass.value.length >minValue && pass.value.match(letters) && pass.value.match(numbers)){
-    p.textContent='Twoje hasło jest mocne';
-    p.style.color='green';
-  }else{
-    p.textContent='Twoje hasło jest słabe';
-    p.style.color='red';
-  }
+const final = function(){
+  if (pass.value !== '') {
+	check();
+} else {
+	p.textContent = "Podaj hasło";
+	p.style.color = "";
 }
+};
 
-pass.addEventListener('keyup',check)
+pass.addEventListener("keyup", final);
