e6504c4221d9a1dbf6d47ff8949843094d5252f2
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-02-18 16:47:52 +0100

    Walidator hasla

---
+++ js.js
@@ -0,0 +1,22 @@
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
+
+
+const check = function(){
+  if(pass.value.length >minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)){
+    p.textContent='Twoje hasło jest bardzo mocne';
+    p.style.color='lime'
+  }else if (pass.value.length >minValue && pass.value.match(letters) && pass.value.match(numbers)){
+    p.textContent='Twoje hasło jest mocne';
+    p.style.color='green';
+  }else{
+    p.textContent='Twoje hasło jest słabe';
+    p.style.color='red';
+  }
+}
+
+pass.addEventListener('keyup',check)
